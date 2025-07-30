3d73643c49102c893e8ab4da46640d91c2a4f7ba
Maria Frydaki <mfrydaki@gmail.com>
2025-07-30 21:19:50 +0300

    updates

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -12,19 +12,16 @@ import { Events } from "../data/events";
 
    return (
      <div className="min-h-screen bg-gradient-to-r from-purple-950 to-indigo-300 flex items-center justify-center p-6 animate-gradient">
-       <div className="bg-white rounded-3xl shadow-xl max-w-3xl w-full overflow-hidden flex flex-col md:flex-row">
-        
-         {/* Εικόνα */}
-         <div className="md:w-1/2 w-full">
+       <div className="bg-white rounded-3xl shadow-xl max-w-5xl w-full overflow-hidden flex-col md:flex-row">
+         {/* Picture */}
+         <div className="p-2 h-full w-full">
            <img
              src={event.image}
              alt={event.title}
-             className="object-cover h-full w-full"
+             className="w-full max-w-[1000px] max-h-[400px] object-cover mx-auto rounded-lg"
            />
          </div>
-
-         {/* Περιγραφή */}
-         <div className="p-6 md:w-1/2 w-full flex flex-col justify-between">
+         <div className="p-2 h-full w-full">
            <div>
              <h1 className="text-2xl font-bold text-indigo-800 mb-4">
                {event.title}
@@ -32,14 +29,15 @@ import { Events } from "../data/events";
              <p className="text-gray-600 mb-2">
                <strong className="text-indigo-700">Date:</strong> {event.date}
              </p>
-             {/* Εδώ μπορείς να προσθέσεις περιγραφή ή περισσότερες λεπτομέρειες */}
-             <p className="text-sm text-gray-500 mt-4">
-               Join us for an inspiring event where amazing women from the STEM
-               world share their journeys, ideas, and knowledge.
+             <p className="text-gray-600 mb-2">
+               <strong className="text-indigo-700"></strong> {event.description}
              </p>
-           </div>
 
-           {/* Κουμπί συμμετοχής */}
+             <p className="text-sm text-gray-500 mt-4"></p>
+           </div>
+           <div className="p-1.5 w-full h-full">{event.details}</div>
+
+           {/* Attend Button */}
            <button className="mt-6 px-4 py-2 bg-indigo-600 text-white rounded-xl hover:bg-indigo-700 transition">
              Attend Event
            </button>
